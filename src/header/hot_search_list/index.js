6e20aaa79470c4b0d25280fd86c019f15ba1fc89
Planisplan <zhuliangqi@gmail.com>
2019-02-22 16:22:02 +0800

    2019/2/22

--- src/header/hot_search_list/index.js
+++ src/header/hot_search_list/index.js
@@ -3,34 +3,34 @@ import {sources} from '../../sources';
 import styles from './index.module.css'
 
 class HotSearchList extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            focusedIndex:null
-        }
-        this.handleMouseOver=this.handleMouseOver.bind(this);
-        this.handleMouseOut=this.handleMouseOut.bind(this)
-    }
+    // constructor(props){
+    //     super(props)
+    //     this.state={
+    //         focusedIndex:null
+    //     }
+    //     this.handleMouseOver=this.handleMouseOver.bind(this);
+    //     this.handleMouseOut=this.handleMouseOut.bind(this)
+    // }
 
-    handleMouseOver(event){
-        this.setState({
-            focusedIndex:Number(event.target.getAttribute('data-index'))
-        })
-    }
+    // handleMouseOver(event){
+    //     this.setState({
+    //         focusedIndex:Number(event.target.getAttribute('data-index'))
+    //     })
+    // }
 
-    handleMouseOut(){
-        this.setState({
-            focusedIndex:null,
-        })
-    }
+    // handleMouseOut(){
+    //     this.setState({
+    //         focusedIndex:null,
+    //     })
+    // }
   
   
     render(){
         return(
-            <div className={styles.searchList} style={{visibility:this.props.status?'inherit':'collapse'}}>
-            <p style={{borderBottom: '1px solid #ebebeb', color:'#8590a6'}}>知乎热搜</p>   
-            <ul onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
-            {sources.hotSearch.map((val,index)=><li key={index} data-index={index} style={{backgroundColor:this.state.focusedIndex==index?'#f6f6f6':''}}>{val}</li>)}
+            <div className={styles.wrapper} style={{visibility:this.props.status?'inherit':'collapse'}}>
+            <p className={styles.title}>知乎热搜</p>   
+            <ul className={styles.list}>
+            {sources.hotSearch.map((val,index)=><li key={index} data-index={index} className={styles.listItem}>{val}</li>)}
             </ul>
         </div>
         )
