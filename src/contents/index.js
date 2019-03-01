06d9a62fce83f5f6e6e274bb5fcdac6fe08d65ef
Planisplan <zhuliangqi@gmail.com>
2019-03-01 16:28:17 +0800

    ajax!!

--- src/contents/index.js
+++ src/contents/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import {sources} from '../sources';
 import styles from './index.module.css';
 import Card from './card';
@@ -7,38 +7,49 @@ import Card from './card';
 class Content extends React.Component {
 	render(){
 		return(
-			<Router><div className={styles.nav}>
+		<div className={styles.wrapper}>
 				<ul className={styles.list}>
-					<li className={styles.listItem}><Link to='/'>推荐</Link></li>
-					<li className={styles.listItem}><Link to='/Follow'>关注</Link></li>
-					<li className={styles.listItem}><Link to='/hot'>热榜</Link></li>
+					<li className={styles.listItem} onClick={()=>{ajax('get','http://localhost:4000/')}}>推荐</li>
+					<li className={styles.listItem} onClick={()=>{ajax('get','http://localhost:4000/follow')}}>关注</li>
+					<li className={styles.listItem} onClick={()=>{ajax('get','http://localhost:4000/recommend')}}>热榜</li>
 				</ul>
-
-			<Route exact path='/' component={Recommend}/>
-			<Route exact path='/Follow' component={Follow}/>
-			<Route exact path='/hot' component={Hot}/>
-			</div>
-		</Router>
-
+				<Recommend/>
+		</div>
 		)
 	}
 }
 
+function ajax (method,url,async=true){
+	const xhr = new XMLHttpRequest();
+	xhr.open(method,url,async)
+	xhr.send(null)
+
+	xhr.onload=success
+
+	function success(){
+		if (xhr.status === 200 || xhr.status === 304) {
+			console.log(xhr.responseText)
+		} else {
+			console.log('error')
+		}
+	}
+}
+
 const Follow = ()=>(<div className={styles.content}>
-	{sources.articles.map((val,index)=>(
-		<Card article={val} img={sources.imgs[index]} title={sources.titles[index]} key={index}/>
+	{sources.articles.texts.map((val,index)=>(
+		<Card article={val} img={sources.articles.imgs[index]} title={sources.articles.titles[index]} key={index}/>
 	))}
 </div>)
 
 const Hot = ()=>(<div className={styles.content}>
-	{sources.articles.map((val,index)=>(
-		<Card article={val} img={sources.imgs[index]} title={sources.titles[index]} key={index}/>
+	{sources.articles.texts.map((val,index)=>(
+		<Card article={val} img={sources.articles.imgs[index]} title={sources.articles.titles[index]} key={index}/>
 	))}
 </div>)
 
 const Recommend = ()=>(<div className={styles.content}>
-	{sources.articles.map((val,index)=>(
-		<Card article={val} img={sources.imgs[index]} title={sources.titles[index]} key={index}/>
+	{sources.articles.texts.map((val,index)=>(
+		<Card article={val} img={sources.articles.imgs[index]} title={sources.articles.titles[index]} key={index}/>
 	))}
 </div>)
 
